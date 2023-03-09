92f047514aa914828972adc22a8a1d811a349850
Evelina <eve1ina@list.ru>
2023-03-10 00:08:05 +0300

    Добавлено 3 фрагмента

--- app/src/main/java/com/example/pr4/Services.java
+++ app/src/main/java/com/example/pr4/Services.java
@@ -4,44 +4,27 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Services#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class Services extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    final String TAG = "ServicesLayout";
+    Button backButton;
     public Services() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Services.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static Services newInstance(String param1, String param2) {
         Services fragment = new Services();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,15 +33,31 @@ public class Services extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_services, container, false);
+        View v = inflater.inflate(R.layout.fragment_services, container, false);
+        Toast.makeText(getActivity(), "onCreateViewAddCar", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onCreateView");
+
+        backButton = (Button) v.findViewById(R.id.GoBackButton);
+
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (savedInstanceState == null) {
+                    getActivity().getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container, FirstScreen.newInstance(null, null))
+                            .commit();
+                }
+            }
+        });
+        return v;
     }
 }
