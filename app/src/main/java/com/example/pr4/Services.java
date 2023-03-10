8c657ec0f23a6accf424c515660d8cf887b8585c
Evelina <eve1ina@list.ru>
2023-03-10 17:29:41 +0300

    Добавлен RecyclerView в фрагмент "Services"

--- app/src/main/java/com/example/pr4/Services.java
+++ app/src/main/java/com/example/pr4/Services.java
@@ -3,20 +3,24 @@ package com.example.pr4;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 
-public class Services extends Fragment {
+public class Services extends Fragment implements ServicesAdapter.OnItemClickListener {
 
     final String TAG = "ServicesLayout";
     Button backButton;
+    RecyclerView services;
     public Services() {
         // Required empty public constructor
     }
@@ -46,6 +50,16 @@ public class Services extends Fragment {
 
         backButton = (Button) v.findViewById(R.id.GoBackButton);
 
+        final String[] servicesArray = getResources().getStringArray(R.array.services);
+
+        // Первым делом необходимо найти список на верстке экрана
+        services = (RecyclerView) v.findViewById(R.id.services);
+        // Далее, создать адаптер и передать в него Context имассив элементов
+        ServicesAdapter adapter = new ServicesAdapter(getContext(), servicesArray, this);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        services.setLayoutManager(layoutManager);
+        // устанавливаем для списка адаптер
+        services.setAdapter(adapter);
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,5 +73,11 @@ public class Services extends Fragment {
             }
         });
         return v;
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        //String selection = ServicesAdapter.items;
+        Toast.makeText(getContext(), "RecyclerView нажатие", Toast.LENGTH_SHORT).show();
     }
 }
